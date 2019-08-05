d4f14411dba7900c3ed6e3acfc817722fe377dda
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-08-04 22:26:08 -0400

    Develop mobile-first site layout with skeleton of css

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 function sizeCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  ctx.fillStyle = "rgba(0, 0, 0)";
+  ctx.fillStyle = "rgba(42, 44, 162)";
   ctx.fillRect(0,0, canvas.width, canvas.height);
 }
 
