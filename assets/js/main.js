426cc9603784e8c45e72a6735847e0a5f2d5cfaa
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-08-19 19:06:43 -0400

    Begin updating project display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,3 @@
-// const canvas = document.getElementById("canvas");
-// const ctx = canvas.getContext("2d");
-// function sizeCanvas() {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-//   ctx.fillStyle = "rgba(42, 44, 162)";
-//   ctx.fillRect(0,0, canvas.width, canvas.height);
-// }
-// sizeCanvas();
-// window.addEventListener("resize", sizeCanvas);
-
 const menuButton = document.getElementById("menu-button");
 menuButton.tabbed = false;
 menuButton.addEventListener("click", activateTabs);
