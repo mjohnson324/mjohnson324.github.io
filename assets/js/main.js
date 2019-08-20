574745dde6c3767e12283b1d7bd5dc738deff48c
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-08-20 15:15:52 -0400

    Update projects section to display only one project at a time

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ function displayTabs() {
   const tabs = document.querySelectorAll("#page-tabs a");
   tabs.forEach(tab => {
     tab.addEventListener("click", hideTabs);
-    tab.classList.remove('nav-display');
+    tab.classList.remove('hidden');
   });
 }
 
@@ -24,7 +24,32 @@ function hideTabs() {
   const tabs = document.querySelectorAll("#page-tabs a");
   tabs.forEach(tab => {
     tab.removeEventListener("click", hideTabs);
-    tab.classList.add('nav-display');
+    tab.classList.add('hidden');
   });
   menuButton.tabbed = false;
+}
+
+const mockdoc = document.getElementById("mockdoc-tab");
+const blockhead = document.getElementById("blockhead-tab");
+const rdcover = document.getElementById("rdcover-tab");
+const tabButtons = [mockdoc, blockhead, rdcover];
+tabButtons.forEach(button => {
+  button.addEventListener("click", cycleProjects);
+});
+
+function cycleProjects(e) {
+  e.preventDefault();
+  const currentID = e.target.id;
+  tabButtons.forEach(button => {
+    let classes = button.classList;
+    let projectID = button.id.split("-")[0];
+    let project = document.getElementById(projectID);
+    if (button.id === currentID && classes.contains("hidden-border")) {
+      button.classList.remove("hidden-border");
+      project.classList.remove("hidden");
+    } else if (button.id !== currentID && classes.contains("hidden-border") === false) {
+      button.classList.add("hidden-border");
+      project.classList.add("hidden");
+    }
+  });
 }
