f7f6e8615490a216065b702cb61a4026db4c2874
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-10-24 15:37:29 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Portfolio
-
-A website showcasing my best work. Now bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.2e7507364e859e17eb4aad0723fa0793.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a145b6c0a7dc4d5afcab66908e50776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e424a000ffb397c4b9",
+    "url": "/static/css/main.e4a51384.chunk.css"
+  },
+  {
+    "revision": "821d616689e109faee24",
+    "url": "/static/js/2.8a63615a.chunk.js"
+  },
+  {
+    "revision": "d2e424a000ffb397c4b9",
+    "url": "/static/js/main.2521fb5c.chunk.js"
+  },
+  {
+    "revision": "12ce529ca47c67856d2e",
+    "url": "/static/js/runtime-main.ca2bac40.js"
+  },
+  {
+    "revision": "64f6eee4d1e93244273c464a2f6037c9",
+    "url": "/static/media/blockhead.64f6eee4.png"
+  },
+  {
+    "revision": "49892cd6f0e10a344e4a2cac7631691c",
+    "url": "/static/media/mockdoc.49892cd6.png"
+  },
+  {
+    "revision": "1b28901afe6ee40c3b678e38e9f461cc",
+    "url": "/static/media/photo.1b28901a.jpg"
+  },
+  {
+    "revision": "1786041ac3dab1228b6a7c72d8f5ae8c",
+    "url": "/static/media/rdcover.1786041a.png"
+  },
+  {
+    "revision": "09d897ca4a7661934e3c928d0003d38c",
+    "url": "/static/media/rest-countries.09d897ca.png"
+  }
+]);
